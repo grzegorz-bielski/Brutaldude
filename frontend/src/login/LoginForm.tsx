0862b65bd529ceeb6a5892b5a5e7269623bd3156
wojciech-wolny <wojciech.wolny@skygate.io>
2018-08-31 22:20:19 +0200

    Fetching token with style

--- frontend/src/login/LoginForm.tsx
+++ frontend/src/login/LoginForm.tsx
@@ -1,42 +1,57 @@
 import * as React from "react";
+import {loginUrl} from '../shared/urls'
 
-interface IState { 
+interface IState {
+    infoStatus: string; 
+    loginCredentials: ILoginCredentials;
+}
+
+interface ILoginCredentials {
     password: string;
     username: string;
 }
 
+interface ILoginResponse {
+    token: string;
+}
 
 class LoginComponent extends React.Component<{}> {
     public state: IState = {
-        password: "", 
-        username: ""
+        infoStatus: "",
+        loginCredentials: {
+            password: "",
+            username:  "",
+        }
     };
 
     public render() {
         return (
             <div className="control">
-            <form onSubmit={this.handleSubmit}>
-            <input className="input is-focused" type="text" placeholder="Username" onChange={this.handleUsernameInput}/>
-            <input className="input is-focused" type="password" placeholder="Password" onChange={this.handlePasswordInput}/>
-            <button className="primary"> Submit </button>
-            </form>
+                <form onSubmit={this.handleSubmit}>
+                    <input className="input is-focused" type="text" placeholder="Username" onChange={this.handleUsernameInput} value={this.state.loginCredentials.username}/>
+                    <input className="input is-focused" type="password" placeholder="Password" onChange={this.handlePasswordInput} value={this.state.loginCredentials.password}/>
+                    <button className="primary"> Submit </button>
+                </form>
             </div>
         ); 
     }
 
     private handleUsernameInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({username: event.target.value});
+        this.setState({loginCredentials: {username: event.target.value, password: this.state.loginCredentials.password}});
     }
 
     private handlePasswordInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({password: event.target.value});
+        this.setState({loginCredentials: {password: event.target.value, username: this.state.loginCredentials.username}});
     }
 
     private handleSubmit = (event: React.KeyboardEvent<HTMLFormElement>) => {
         event.preventDefault();
-        if(this.state.password === "admin" && this.state.username === "admin"){
-            console.log("Jestem Ściekiem");
-        }
+        fetch(
+            loginUrl, {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(this.state.loginCredentials)}
+        ).then( (response: Response) => response.json() )
+        .then( (responseData: ILoginResponse) => {localStorage.setItem('JTW-Token', responseData.token)} )
+        .catch( () => this.setState({infoStatus: 'error'}) )
+        this.setState({loginCredentials: {username: "", password: ""}})
     }
 }
 
